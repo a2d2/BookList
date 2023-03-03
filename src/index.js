e6d990afaab59d66b029006cddf91ce65214a89f
r2d2 <arturo.galvis@hotmail.com>
2023-03-03 11:24:07 -0500

    event-basics

--- src/index.js
+++ src/index.js
@@ -20,16 +20,40 @@ const books = [
 function BookList() {
   return (
     <section className="booklist">
+      <EventExamples />
       {books.map((book) => {
-        {
-          /* const { img, title, author, id } = book;
-        return <Book img={img} title={title} author={author} key={id} />; */
-        }
+        /* {
+          const { img, title, author, id } = book;
+        return <Book img={img} title={title} author={author} key={id} />; 
+        }*/
         return <Book {...book} key={book.id} />; //spread operator
       })}
     </section>
   );
 }
+
+const EventExamples = () => {
+  const handleFormInput = () => {
+    console.log('handle form input');
+  };
+  const handleButtonClick = () => {
+    alert('handle button click');
+  };
+  return (
+    <section>
+      <form>
+        <h2>Typical Form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={handleFormInput}
+          style={{ margin: '1rem 0' }}
+        />
+      </form>
+      <button onClick={handleButtonClick}>click me</button>
+    </section>
+  );
+};
 
 const Book = (props) => {
   const { img, author, title } = props;
