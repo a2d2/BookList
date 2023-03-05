32db9834f2da7193f726846c6eaa033bcbf7661a
r2d2 <arturo.galvis@hotmail.com>
2023-03-04 19:01:26 -0500

    8-example

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+// create getBook function in booklist, accepts id as an argument and finds the book
+// Javascript Nuggets - Filter and Find
+// pass the function down to Book Component and invoke on the button click in the Book Component destructure id and function invoke the function when user clicks the button, pass the id
+// the goal : you should see the same book in the console
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -18,34 +23,40 @@ const books = [
 ];
 
 function BookList() {
-  const someValue = 'shakeAndBake';
-  const displayValue = () => {
-    console.log(someValue);
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id);
+    console.log(book);
   };
+  //getBook(2);
+
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book {...book} key={book.id} displayValue={displayValue} />; //spread operator
+        return <Book {...book} key={book.id} getBook={getBook} />; //spread operator
       })}
     </section>
   );
 }
 
 const Book = (props) => {
-  const { img, author, title, displayValue } = props;
-  // const displayTitle = () => {
-  //   console.log(title);
+  const { img, author, title, getBook, id } = props;
+  // const getSingleBook = () => {
+  //   getBook(id);
   // };
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author} </h4>
-      {/* Anonymous function instead of calling the function "displayTitle" like onClick={ displayTitle} we execute the function inside the event */}
       <button onClick={() => console.log(title)}>display title</button>
       <div>
-        {/* As far as data flow, we can only pass props down from the parent to the child */}
-        <button onClick={displayValue}>click me</button>
+        {/* this will not work since we will always be executing onClick function */}
+        {/* <button onClick={getBook(id)}>click me</button> */}
+        {/* One way to solve the issue is by doing the following plus adding the funtion above called getSingleBook
+        <button onClick={getSingleBook}>click me</button> */}
+        {/* Or we can include the function inside the event in one single line as
+        this: */}
+        <button onClick={() => getBook(id)}>click me</button>
       </div>
     </article>
   );
