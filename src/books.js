a910ed4cf240659b4d5d45cf50d7c3c91ca46137
r2d2 <arturo.galvis@hotmail.com>
2023-03-04 19:10:00 -0500

    8-example

---
+++ src/books.js
@@ -0,0 +1,16 @@
+export const books = [
+  {
+    author: 'El Trollino',
+    title: 'HORA DE MIKELLINO: NUESTRA PRIMERA NOCHE',
+    img: 'https://panamericana.vtexassets.com/arquivos/ids/481626-1200-auto?v=638114855532200000&width=1200&height=auto&aspect=true',
+    id: 1,
+  },
+  {
+    author: 'Antonie Saint exupery',
+    title: 'EL PRINCIPITO',
+    img: 'https://panamericana.vtexassets.com/arquivos/ids/484839-1200-auto?v=638129391986400000&width=1200&height=auto&aspect=true',
+    id: 2,
+  },
+];
+
+//export default books;
--- src/index.js
+++ src/index.js
@@ -6,21 +6,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
-const books = [
-  {
-    author: 'El Trollino',
-    title: 'HORA DE MIKELLINO: NUESTRA PRIMERA NOCHE',
-    img: 'https://panamericana.vtexassets.com/arquivos/ids/481626-1200-auto?v=638114855532200000&width=1200&height=auto&aspect=true',
-    id: 1,
-  },
-  {
-    author: 'Antonie Saint exupery',
-    title: 'EL PRINCIPITO',
-    img: 'https://panamericana.vtexassets.com/arquivos/ids/484839-1200-auto?v=638129391986400000&width=1200&height=auto&aspect=true',
-    id: 2,
-  },
-];
+import { books } from './books';
+//import books from './books';
 
 function BookList() {
   const getBook = (id) => {
